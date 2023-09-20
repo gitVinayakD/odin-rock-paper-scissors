0d0c23ff5bbed416691b1eb5301fe584a2534f08
gitVinayakD <49040439+gitVinayakD@users.noreply.github.com>
2023-09-20 12:26:25 -0700

    Finished game by adding game() function.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let winCount = 0;
+let loseCount = 0;
+
 function getComputerChoice(){
     let choice;
     let a = Math.round(Math.random()*2);
@@ -19,29 +22,50 @@ function playRound(playerSelection, computerSelection) {
         if (lowerComp === 'rock'){
             return "It's a tie! No one wins";
         }else if (lowerComp === 'scissors'){
+            winCount++;
             return "You win! Rock beats Scissors";
         }else if (lowerComp === 'paper'){
+            loseCount++;
             return "You lose! Paper beats Rock";
         }
     }else if (lowerPlayer === 'paper'){
         if (lowerComp === 'rock'){
+            winCount++;
             return "You win! Paper beats Rock";
         }else if (lowerComp === 'scissors'){
+            loseCount++;
             return "You lose! Scissors beats Paper";
         }else if (lowerComp === 'paper'){
             return "It's a tie! No one wins";
         }
     }else if(lowerPlayer === 'scissors'){
         if (lowerComp === 'rock'){
+            loseCount++;
             return "You lose! Rock beats Scissors";
         }else if (lowerComp === 'scissors'){
             return "It's a tie! No one wins";
         }else if (lowerComp === 'paper'){
+            winCount++;
             return "You win! Scissors beats Paper";
         }
     }
 }
-   
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+
+
+function game(){
+    for (let i=1;i<=5;i++){
+        let playerInput = prompt("Rock, Paper, or Scissors?");
+        console.log(playRound(playerInput, getComputerChoice()));
+        console.log("Wins:"+winCount+", Losses:"+loseCount)  
+    }
+    if(winCount>loseCount){
+        console.log("You win! "+winCount+ " to "+ loseCount);
+    }else if (loseCount>winCount){
+        console.log("You lose! "+winCount+ " to "+ loseCount);
+    }else{
+        console.log("You tie! "+winCount+ " to "+ loseCount);
+    }
+}
+
+game();
