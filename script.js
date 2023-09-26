ea228f52c0520819b1427e55d23e7527b5f7d1b6
gitVinayakD <49040439+gitVinayakD@users.noreply.github.com>
2023-09-25 18:40:14 -0700

    Finished adding updates that use Javascript to use buttons to take in user input and update win lose text on screen

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let winCount = 0;
 let loseCount = 0;
 
+const container = document.querySelector('#result');
+
+const roundResult = document.createElement('div');
+roundResult.className="roundResult";
+roundResult.textContent = "No rounds played yet!";
+container.appendChild(roundResult);
+
+const currentScore = document.createElement('div');
+currentScore.className="currentScore";
+currentScore.textContent = "Wins: "+winCount+", Losses: "+loseCount;
+container.appendChild(currentScore);
+
+
+
 function getComputerChoice(){
     let choice;
     let a = Math.round(Math.random()*2);
@@ -11,7 +25,6 @@ function getComputerChoice(){
     }else{
         choice = 'Scissors';
     }
-    console.log(a +" : "+choice);
     return choice;
 }
 
@@ -20,52 +33,80 @@ function playRound(playerSelection, computerSelection) {
     const lowerComp = computerSelection.toLowerCase();
     if (lowerPlayer === 'rock'){
         if (lowerComp === 'rock'){
-            return "It's a tie! No one wins";
+            return "This round is a tie! No one wins";
         }else if (lowerComp === 'scissors'){
             winCount++;
-            return "You win! Rock beats Scissors";
+            return "You win this round! Rock beats Scissors";
         }else if (lowerComp === 'paper'){
             loseCount++;
-            return "You lose! Paper beats Rock";
+            return "You lose this round! Paper beats Rock";
         }
     }else if (lowerPlayer === 'paper'){
         if (lowerComp === 'rock'){
             winCount++;
-            return "You win! Paper beats Rock";
+            return "You win this round! Paper beats Rock";
         }else if (lowerComp === 'scissors'){
             loseCount++;
-            return "You lose! Scissors beats Paper";
+            return "You lose this round! Scissors beats Paper";
         }else if (lowerComp === 'paper'){
-            return "It's a tie! No one wins";
+            return "This round is a tie! No one wins";
         }
     }else if(lowerPlayer === 'scissors'){
         if (lowerComp === 'rock'){
             loseCount++;
-            return "You lose! Rock beats Scissors";
+            return "You lose this round! Rock beats Scissors";
         }else if (lowerComp === 'scissors'){
-            return "It's a tie! No one wins";
+            return "This round is a tie! No one wins";
         }else if (lowerComp === 'paper'){
             winCount++;
-            return "You win! Scissors beats Paper";
+            return "You win this round! Scissors beats Paper";
         }
     }
 }
 
 
 
-function game(){
-    for (let i=1;i<=5;i++){
-        let playerInput = prompt("Rock, Paper, or Scissors?");
-        console.log(playRound(playerInput, getComputerChoice()));
-        console.log("Wins:"+winCount+", Losses:"+loseCount)  
-    }
-    if(winCount>loseCount){
-        console.log("You win! "+winCount+ " to "+ loseCount);
-    }else if (loseCount>winCount){
-        console.log("You lose! "+winCount+ " to "+ loseCount);
-    }else{
-        console.log("You tie! "+winCount+ " to "+ loseCount);
+function game(input){
+    roundResult.textContent = playRound(input, getComputerChoice());
+    
+    if(winCount==5){
+        currentScore.textContent = "You win! "+winCount+ " rounds to "+ loseCount+ " rounds";
+        setFinalResultsMode();
+    }else if (loseCount==5){
+        currentScore.textContent ="You lose! "+winCount+ " rounds to "+ loseCount+ " rounds";
+        setFinalResultsMode();
+    }else {
+        currentScore.textContent ="Wins: "+winCount+", Losses: "+loseCount;  
     }
 }
 
-game();
+function setFinalResultsMode(){
+    const options = document.querySelector('#options');
+    options.style.setProperty('display','none');
+    const restart = document.createElement('button');
+    restart.className="restart";
+    restart.textContent = "Restart?";
+    container.appendChild(restart);
+    restart.addEventListener('click', () => {
+        options.style.removeProperty('display');
+        restart.style.setProperty('display','none');
+        winCount = 0;
+        loseCount = 0;
+        roundResult.textContent = "No rounds played yet!";
+        currentScore.textContent = "Wins: "+winCount+", Losses: "+loseCount;
+        }
+    );
+
+}
+
+
+let playerInput = '';
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerInput = button.id;
+        game(playerInput);
+        }
+    );
+    }
+);
